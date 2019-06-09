26f96bd37d0bcb12623839d6e5c882e299c62dd5
Karl Broman <kbroman@gmail.com>
2019-06-09 14:27:09 -0700

    Add caTools and rmarkdown to install.R (for RStudio version)

--- install.R
+++ install.R
@@ -1,4 +1,4 @@
-install.packages("devtools")
+install.packages(c("devtools", "rmarkdown", "caTools"))
 devtools::install_github("rqtl/qtl2")
 devtools::install_github("rqtl/GNapi")
 devtools::install_github("rqtl/qtl2browse")
